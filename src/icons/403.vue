d632d2f308c4e34d56524624c672a2b0603c413b
qifang280 <qifang280@gmail.com>
2023-09-25 16:42:13 +0800

    gpt自定义 - 登录图片修改

--- src/icons/403.vue
+++ src/icons/403.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="text-[#142D6E] dark:text-[#3a71ff]">
-		<img src="src/assets/bot.jpg" alt="">
+		<img class="food-thumb"  src="/src/assets/bot.jpg"  alt="">
 	</div>
 </template>
